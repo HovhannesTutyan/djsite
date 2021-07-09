af3ad916a37d49ffb663170a2c12a174d54ccfea
Admin <atutyanaz@gmail.com>
2021-07-09 19:56:16 +0400

    adding url addresses in templates

--- coolsite/woman/views.py
+++ coolsite/woman/views.py
@@ -2,17 +2,28 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 
-menu=['About this site', 'Add an article', 'Feedback', 'Enter']
+menu=[{'title': 'About this site', 'url_name':'about'},
+      {'title': 'Add an article', 'url_name':'add_page'},
+      {'title':'Feedback', 'url_name':'contact'},
+      {'title':'Enter','url_name':'login'}]
 
 
 def index(request):
     posts=Woman.objects.all()
-    return render(request,'woman/index.html', {'posts':posts,'menu': menu, "title":"Главная страница"})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title':'Main Page'
+    }
+    return render(request,'woman/index.html', context=context)
 def about(request):
     return render(request,'woman/about.html', {'menu': menu, 'title':"About Page"})
-def categories(request, catslug):
-    if (request.GET):
-        print(request.GET)
-    return HttpResponse(f"<h1> Articles by categories </h1> <p> {catslug} </p>")
-def archive(request, year):
-    return HttpResponse(f"<h1> Articles by year </h1> <p> {year} </p>")
+
+def addpage(request):
+    return HttpResponse("Add articles")
+def contact(request):
+    return HttpResponse("Contact us")
+def login(request):
+    return HttpResponse("Login")
+def show_post(request, post_id):
+    return HttpResponse(f"Showing posts with id = {post_id}")
